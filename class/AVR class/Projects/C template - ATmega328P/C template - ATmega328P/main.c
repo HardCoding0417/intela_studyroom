77806aa3311bec88e339d8e49d430d74776de983
13576 <happybox2@naver.com>
2023-08-14 17:26:55 +0900

    📦 Add microchip studio template

---
+++ class/AVR class/Projects/C template - ATmega328P/C template - ATmega328P/main.c
@@ -0,0 +1,35 @@
+/*
+ * 
+ *
+ * Created: 
+ * Author : Choi Myeongsu
+ */ 
+
+
+
+
+#include <avr/io.h>
+
+#define F_CPU 16000000
+#include <util/delay.h>
+#define DELAY_MS 30.
+
+#include <stdbool.h>
+
+
+#define LED_DDR DDRD
+#define LED_PORT PORTD
+
+
+
+int main() {
+	LED_DDR = 0xff;
+	
+    while (true) {
+		LED_PORT = 0xff;
+		_delay_ms(DELAY_MS);
+		LED_PORT = 0x00;
+		_delay_ms(DELAY_MS);
+    }
+}
+
