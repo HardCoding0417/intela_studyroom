e7a82ba5fad9b1e1b6b2e8ab792cbb4c94019717
13576 <happybox2@naver.com>
2023-08-20 20:49:30 +0900

    ♻️ Refactor CTC Mode practice code

--- class/AVR class/Projects/TimerCounter/PWM/main.c
+++ class/AVR class/Projects/TimerCounter/PWM/main.c
@@ -1,22 +1,21 @@
 /*
- * PWM.c
+ * CTC Mode practice
  *
  * Created: 2023-08-18 오후 2:47:07
- * Author : user
+ * Author : Choi Myeongsu
  */ 
 
 #include <avr/io.h>
-#include <avr/interrupt.h>
 
 
 int main(void)
 {
-	TCCR0 |= 0x1 << CS02 | 0x0 << CS01 | 0x0 << CS00; // 64
+	TCCR0 |= 0x1 << CS02 | 0x0 << CS01 | 0x0 << CS00; // the prescaler divisor: 64
 	TCCR0 |= 0x1 << WGM01 | 0x0 << WGM00;
 	TCCR0 |= 0x0 << COM01 | 0x1 << COM00;
 	OCR0 = 124; // for 1000Hz OC0
 	
-	DDRB = 0x10;
+	DDRB = 0x10; // PB4(OC0)
 	
     while (1) 
     {
