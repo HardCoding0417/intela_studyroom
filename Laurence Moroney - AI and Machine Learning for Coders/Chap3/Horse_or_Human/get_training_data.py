53d76782261296206275f5e7f524e3a4fcac417c
13576 <happybox2@naver.com>
2023-10-04 08:58:41 +0900

    🚧 'AI ... for Coders' in progress

--- Laurence Moroney - AI and Machine Learning for Coders/Chap3/Horse_or_Human/get_training_data.py
+++ Laurence Moroney - AI and Machine Learning for Coders/Chap3/Horse_or_Human/get_training_data.py
@@ -7,8 +7,8 @@ url = 'https://storage.googleapis.com/laurencemoroney-blog.appspot.com/'
 file_name = 'horse-or-human.zip'
 training_dir = './dataset/'
 
-# urllib.request.urlretrieve(url + file_name, file_name)
+urllib.request.urlretrieve(url + file_name, file_name)
 
-# zip_ref = zipfile.ZipFile(file_name, 'r')
-# zip_ref.extractall(training_dir)
-# zip_ref.close()
+zip_ref = zipfile.ZipFile(file_name, 'r')
+zip_ref.extractall(training_dir)
+zip_ref.close()
