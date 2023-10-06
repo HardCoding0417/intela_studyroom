8ce494d7984d73a0d401fd7895aa6ba96cde0fa0
HardCoding0417 <140784427+HardCoding0417@users.noreply.github.com>
2023-10-07 02:33:02 +0900

    1

---
+++ Moon_jeangHwan/Crawler/leading_crawler/crawlers/crawler.py
@@ -0,0 +1,31 @@
+import requests
+from urllib.robotparser import RobotFileParser
+from bs4 import BeautifulSoup
+import logging
+
+# 로깅 설정
+logging.basicConfig(level=logging.INFO, 
+                    format='%(asctime)s - %(levelname)s - %(message)s', 
+                    filename="crawler.log")
+
+# robots.txt를 준수하기 위한 함수
+def check_can_fetch(url, user_agent):   
+    rp = RobotFileParser()
+    rp.set_url(url + "/robots.txt")
+    rp.read()
+    return rp.can_fetch(user_agent, url)
+
+# 접근
+def fetch_page(url, user_agent):
+    if not check_can_fetch(url, user_agent):
+        logging.warning(f"해당 웹페이지에 대한 접근을 허가 받지 못함: {url}")
+        return None
+    response = requests.get(url)
+    return response
+
+# 파싱
+def parse_page(url, user_agent): 
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    return soup
+
